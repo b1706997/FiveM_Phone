030aacb79b3b93472431290219a6452186204541
Nguyen Nhat Son <nguyennhatson1810@gmail.com>
2020-09-01 15:25:58 +0700

    thirdtime

--- phone2.lua
+++ phone2.lua
@@ -33,19 +33,20 @@ RegisterNUICallback('path', function(data)
 end)
 
 RegisterCommand('path', function() 
-    SendDuiMessage(duiObj,{type='getpath'})
+    send_dui({})
 end)
 
--- function send_nui(data)
---     SendDuiMessage(data)
---     print(data)
--- end
+function send_dui(data)
+    SendDuiMessage(duiObj,json.encode(data))
+    -- print(data)
+end
 
 function open_phone()
     CreateMobilePhone(0)
-    SendDuiMessage(duiObj,json.encode({
-        type='open'
-    }))
+    -- SendDuiMessage(duiObj,json.encode({
+    --     type='open'
+    -- }))
+    send_dui({type='open'})
     phone = true
     print(phone)
 end
@@ -62,66 +63,72 @@ end
 
 function close_phone()
     DestroyMobilePhone()
-    SendDuiMessage(duiObj,json.encode({
-        type='close'
-    }))
+    -- SendDuiMessage(duiObj,json.encode({
+    --     type='close'
+    -- }))
+    send_dui({type='close'})
     Wait(500)
     phone = false
     print(phone)
 end
 
 CreateThread(function() 
-    while true do Wait(0)
-        -- Phone condition
+    -- While the phone is turn on do these
+    while true do Wait(0) 
         if(phone==true) then
+            print('phone')
+        -- Phone condition
             -- DisableControlAction(1, 200, true)
             -- DisableControlAction(1, 199, true)
             if GetFollowPedCamViewMode() == 4 then
-				SetMobilePhoneScale(Floatify(0))
-			else
-				SetMobilePhoneScale(Floatify(300))
-			end
+                SetMobilePhoneScale(Floatify(0))
+            else
+                SetMobilePhoneScale(Floatify(300))
+            end
             SetPauseMenuActive(false)
             if(IsControlJustPressed(3, 172)) then -- UP
-                SendDuiMessage(duiObj,json.encode({type='navigate',content='up'}))
+                send_dui({type='navigate',content='up'})
                 CellCamMoveFinger(1)
                 print('up')
             end
             if(IsControlJustPressed(3, 173)) then -- DOWN
-                SendDuiMessage(duiObj,json.encode({type='navigate',content='down'}))
+                send_dui({type='navigate',content='down'})
                 CellCamMoveFinger(2)
                 print('down')
             end
             if(IsControlJustPressed(3, 174)) then  --LEFT
-                SendDuiMessage(duiObj,json.encode({type='navigate',content='left'}))
+                send_dui({type='navigate',content='left'})
                 CellCamMoveFinger(3)
                 print('left')
             end
             if(IsControlJustPressed(3, 175)) then -- RIGHT
-                SendDuiMessage(duiObj,json.encode({type='navigate',content='right'}))
+                send_dui({type='navigate',content='right'})
                 CellCamMoveFinger(4)
                 print('right')
             end
             if(IsControlJustPressed(3, 176)) then -- SELECT
-                SendDuiMessage(duiObj,json.encode({type='navigate',content='select'}))
+                send_dui({type='navigate',content='select'})
                 CellCamMoveFinger(5)
                 print('select')
             end
             if(IsControlJustPressed(1, 194)) then -- CANCLE
-                SendDuiMessage(duiObj,json.encode({type='navigate',content='cancle'}))
+                send_dui({type='navigate',content='cancle'})
                 CellCamMoveFinger(3)
                 print('cancle')
             end
+
+            
 
             -- Render phone
             local phoneRenderId = GetMobilePhoneRenderId()
             SetTextRenderId(phoneRenderId)
             DrawSprite('phone', 'playerPhone', 0.5, 0.5, 1.0, 1.0, 0, 255, 255, 255, 255)
-        end
+        end    
     end
 end)
 
 CreateThread(function() 
+    -- Turn on/off phone listener
     while true do 
         Wait(0)
         if(IsControlJustPressed(1, PhoneOpenButt)) then
@@ -130,8 +137,7 @@ CreateThread(function()
         if(IsControlJustPressed(3, 177)) then
             close_phone()
         end
-        SetPauseMenuActive(false)
-
+        -- SetPauseMenuActive(false)
         -- print(phone)
     end
 end)
